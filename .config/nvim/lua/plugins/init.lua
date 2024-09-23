8dd473dce3136ec2a31edd4068b8ccdb1900785e
Loc Le <lpl212757@gmail.com>
2024-09-23 21:09:29 +0700

    feat - split nvim treesitter out

--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -5,16 +5,5 @@ return {
         config = function()
             require "configs.lspconfig"
         end,
-    },
-
-    {
-        "nvim-treesitter/nvim-treesitter",
-        opts = {
-            ensure_installed = {
-                "vim", "lua", "vimdoc",
-                "html", "css", "c_sharp", "python",
-                "json"
-            },
-        },
-    },
+    }
 }
