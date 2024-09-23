769a9b30cd1e7279c7ebc715ca76797886dfdbc8
Loc Le <lpl212757@gmail.com>
2024-09-23 19:33:02 +0700

    feat - update lspconfig

--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -1,10 +1,32 @@
 -- load defaults i.e lua_lsp
+require("nvchad.lspconfig").defaults()
+
 local lspconfig = require "lspconfig"
 
 -- EXAMPLE
-local servers = { "lua_ls" }
+local servers = { "emmet_language_server", "ts_ls", "html", "cssls", "omnisharp", "jsonls", "pylsp" }
+local nvlsp = require "nvchad.lspconfig"
 
 -- lsps with default config
 for _, lsp in ipairs(servers) do
-  lspconfig[lsp].setup { }
+  lspconfig[lsp].setup {
+    on_attach = nvlsp.on_attach,
+    on_init = nvlsp.on_init,
+    capabilities = nvlsp.capabilities,
+  }
 end
+
+local pid = vim.fn.getpid()
+local util = require "lspconfig/util"
+
+lspconfig.omnisharp.setup {
+  on_attach = nvlsp.on_attach,
+  capabilities = nvlsp.capabilities,
+  cmd = {
+    "/home/lpl212757/.local/share/nvim/mason/packages/omnisharp/omnisharp",
+    "--languageserver",
+    "--hostPID",
+    tostring(pid),
+  },
+  root_dir = util.root_pattern("*.sln", "*.csproj"),
+}
