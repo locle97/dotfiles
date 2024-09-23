6549d7c4bc6b151b799a017ec563ca65fc05e3ee
Loc Le <lpl212757@gmail.com>
2024-09-23 19:44:37 +0700

    feat - update cmp

--- .config/nvim/lua/configs/cmp.lua
+++ .config/nvim/lua/configs/cmp.lua
@@ -1,5 +1,39 @@
 local cmp = require "cmp"
 
+local cmp_ui = {
+    icons = true,
+    lspkind_text = true,
+    style = "flat_dark",   -- default/flat_light/flat_dark/atom/atom_colored
+}
+local cmp_style = cmp_ui.style
+
+local field_arrangement = {
+  atom = { "kind", "abbr", "menu" },
+  atom_colored = { "kind", "abbr", "menu" },
+}
+
+
+local formatting_style = {
+  -- default fields order i.e completion word + item.kind + item.kind icons
+  fields = field_arrangement[cmp_style] or { "abbr", "kind", "menu" },
+
+  format = function(_, item)
+    local icons = require "nvchad.icons.lspkind"
+    local icon = (cmp_ui.icons and icons[item.kind]) or ""
+
+    if cmp_style == "atom" or cmp_style == "atom_colored" then
+      icon = " " .. icon .. " "
+      item.menu = cmp_ui.lspkind_text and "   (" .. item.kind .. ")" or ""
+      item.kind = icon
+    else
+      icon = cmp_ui.lspkind_text and (" " .. icon .. " ") or icon
+      item.kind = string.format("%s %s", icon, cmp_ui.lspkind_text and item.kind or "")
+    end
+
+    return item
+  end,
+}
+
 local function border(hl_name)
   return {
     { "╭", hl_name },
@@ -35,6 +69,8 @@ local options = {
     end,
   },
 
+  formatting = formatting_style,
+
   mapping = {
     ["<C-p>"] = cmp.mapping.select_prev_item(),
     ["<C-n>"] = cmp.mapping.select_next_item(),
@@ -69,6 +105,7 @@ local options = {
     end, { "i", "s" }),
   },
   sources = {
+    -- Default source
     { name = "nvim_lsp" },
     { name = "luasnip" },
     { name = "buffer" },
