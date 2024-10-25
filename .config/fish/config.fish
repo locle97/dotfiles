ebf20c2091a8a31ba9c990627f7aeccb21aae29f
Loc Le <lpl212757@gmail.com>
2024-10-26 00:02:41 +0700

    using bat to search

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,7 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-
+    source ~/.config/fish/shell.fish
+    source ~/.config/fish/init.fish
+    source ~/.config/fish/aliases.fish
 end
 
-source ~/.config/fish/shell.fish
-source ~/.config/fish/init.fish
-source ~/.config/fish/aliases.fish
