36475d2627ca6153a0c4dab58abd2e9f3a14e60d
Loc Le <lpl212757@gmail.com>
2026-05-10 00:06:24 +0700

    chore(terminal): switch default terminal from ghostty to wezterm
    Add .wezterm.lua with Catppuccin Mocha theme, font size 16, disabled
    tab bar, and 0.95 background opacity. Update Hypr keybind variable
    $term to launch wezterm instead of ghostty.

---
+++ .wezterm.lua
@@ -0,0 +1,16 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+
+-- This will hold the configuration.
+local config = wezterm.config_builder()
+
+-- Font
+config.font_size = 16
+config.color_scheme = 'Catppuccin Mocha'
+
+config.enable_wayland = false
+config.enable_tab_bar = false
+config.window_background_opacity = 0.95
+
+-- Finally, return the configuration to wezterm:
+return config
